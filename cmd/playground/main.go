9b5627bf1469794db57a48a81c695d2b02e08b1f
Chase Pierce <syntaqx@gmail.com>
2023-12-14 18:34:51 -0700

    simple github login

---
+++ cmd/playground/main.go
@@ -0,0 +1,135 @@
+// Uses golang.org/x/oauth2/github to simplify the workflow of oauth2 with
+// GitHub.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	// These should be taken from your GitHub application settings at
+	// https://github.com/settings/developers
+	githubClientID := os.Getenv("GITHUB_CLIENT_ID")
+	githubClientSecret := os.Getenv("GITHUB_CLIENT_SECRET")
+
+	if len(githubClientID) == 0 || len(githubClientSecret) == 0 {
+		log.Fatal("Set GITHUB_CLIENT_* env vars")
+	}
+
+	addr := "localhost:8080"
+	callbackPath := "/github/callback/"
+
+	// Note: github auth doesn't support PKCE verification, so we
+	// don't need oauth2.GenerateVerifier.
+	// See https://www.rfc-editor.org/rfc/rfc7636.html for what it is
+	conf := &oauth2.Config{
+		ClientID:     githubClientID,
+		ClientSecret: githubClientSecret,
+		Scopes:       []string{},
+		Endpoint:     github.Endpoint,
+	}
+	lf := &loginFlow{
+		conf: conf,
+	}
+
+	http.HandleFunc("/", lf.rootHandler)
+	http.HandleFunc("/login/", lf.githubLoginHandler)
+	http.HandleFunc(callbackPath, lf.githubCallbackHandler)
+
+	fmt.Printf("Listening on: http://%s\n", addr)
+	log.Panic(http.ListenAndServe(addr, nil))
+}
+
+const rootHTML = `
+<h1>My web app</h1>
+<p>Using the x/oauth2 package</p>
+<p>You can log into this app with your GitHub credentials:</p>
+<p><a href="/login/">Log in with GitHub</a></p>
+`
+
+type loginFlow struct {
+	conf *oauth2.Config
+}
+
+func (lf *loginFlow) rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, rootHTML)
+}
+
+func (lf *loginFlow) githubLoginHandler(w http.ResponseWriter, r *http.Request) {
+	// Generate a random state for CSRF protection and set it in a cookie.
+	state, err := randString(16)
+	if err != nil {
+		panic(err)
+	}
+
+	c := &http.Cookie{
+		Name:     "state",
+		Value:    state,
+		Path:     "/",
+		MaxAge:   int(time.Hour.Seconds()),
+		Secure:   r.TLS != nil,
+		HttpOnly: true,
+	}
+	http.SetCookie(w, c)
+
+	redirectURL := lf.conf.AuthCodeURL(state)
+	http.Redirect(w, r, redirectURL, http.StatusMovedPermanently)
+}
+
+func (lf *loginFlow) githubCallbackHandler(w http.ResponseWriter, r *http.Request) {
+	state, err := r.Cookie("state")
+	if err != nil {
+		http.Error(w, "state not found", http.StatusBadRequest)
+		return
+	}
+	if r.URL.Query().Get("state") != state.Value {
+		http.Error(w, "state did not match", http.StatusBadRequest)
+		return
+	}
+
+	code := r.URL.Query().Get("code")
+	tok, err := lf.conf.Exchange(context.Background(), code)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// This client will have a bearer token to access the GitHub API on
+	// the user's behalf.
+	client := lf.conf.Client(context.Background(), tok)
+	resp, err := client.Get("https://api.github.com/user")
+	if err != nil {
+		panic(err)
+	}
+	respbody, _ := io.ReadAll(resp.Body)
+	userInfo := string(respbody)
+
+	w.Header().Set("Content-type", "application/json")
+	fmt.Fprint(w, string(userInfo))
+}
+
+// randString generates a random string of length n and returns its
+// base64-encoded version.
+func randString(n int) (string, error) {
+	buf := make([]byte, n)
+	if _, err := rand.Read(buf); err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString(buf), nil
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/syntaqx/playground
+
+go 1.21.5
+
+require (
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/oauth2 v0.15.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
