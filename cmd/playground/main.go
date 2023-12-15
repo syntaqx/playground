292e6ba2c50f6aad95378e55fc63b8e7f4e081be
Chase Pierce <syntaqx@gmail.com>
2023-12-14 18:57:01 -0700

    use goth for multi provider

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -1,130 +1,67 @@
 package main
 
 import (
-	"context"
-	"crypto/rand"
-	"encoding/base64"
 	"fmt"
-	"io"
-	"log"
+	"html/template"
 	"net/http"
 	"os"
-	"time"
 
+	"log"
+
+	"github.com/gorilla/pat"
+	"github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/github"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/github"
 )
 
 func main() {
 	_ = godotenv.Load()
 
-	// These should be taken from your GitHub application settings at
-	// https://github.com/settings/developers
-	githubClientID := os.Getenv("GITHUB_CLIENT_ID")
-	githubClientSecret := os.Getenv("GITHUB_CLIENT_SECRET")
+	key := "Secret-session-key" // Replace with your SESSION_SECRET or similar
+	maxAge := 86400 * 30        // 30 days
+	isProd := false             // Set to true when serving over https
 
-	if len(githubClientID) == 0 || len(githubClientSecret) == 0 {
-		log.Fatal("Set GITHUB_CLIENT_* env vars")
-	}
+	store := sessions.NewCookieStore([]byte(key))
+	store.MaxAge(maxAge)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true // HttpOnly should always be enabled
+	store.Options.Secure = isProd
 
-	addr := "localhost:8080"
-	callbackPath := "/github/callback/"
+	gothic.Store = store
 
-	// Note: github auth doesn't support PKCE verification, so we
-	// don't need oauth2.GenerateVerifier.
-	// See https://www.rfc-editor.org/rfc/rfc7636.html for what it is
-	conf := &oauth2.Config{
-		ClientID:     githubClientID,
-		ClientSecret: githubClientSecret,
-		Scopes:       []string{},
-		Endpoint:     github.Endpoint,
-	}
-	lf := &loginFlow{
-		conf: conf,
-	}
+	goth.UseProviders(
+		github.New(os.Getenv("GITHUB_CLIENT_ID"), os.Getenv("GITHUB_CLIENT_SECRET"), "http://localhost:8080/auth/github/callback", "user:email", "repo"),
+	)
 
-	http.HandleFunc("/", lf.rootHandler)
-	http.HandleFunc("/login/", lf.githubLoginHandler)
-	http.HandleFunc(callbackPath, lf.githubCallbackHandler)
+	p := pat.New()
 
-	fmt.Printf("Listening on: http://%s\n", addr)
-	log.Panic(http.ListenAndServe(addr, nil))
-}
+	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+		user, err := gothic.CompleteUserAuth(res, req)
+		if err != nil {
+			fmt.Fprintln(res, err)
+			return
+		}
+		t, _ := template.ParseFiles("templates/success.html")
+		t.Execute(res, user)
+	})
 
-const rootHTML = `
-<h1>My web app</h1>
-<p>Using the x/oauth2 package</p>
-<p>You can log into this app with your GitHub credentials:</p>
-<p><a href="/login/">Log in with GitHub</a></p>
-`
+	p.Get("/logout/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		gothic.Logout(res, req)
+		res.Header().Set("Location", "/")
+		res.WriteHeader(http.StatusTemporaryRedirect)
+	})
 
-type loginFlow struct {
-	conf *oauth2.Config
-}
+	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		gothic.BeginAuthHandler(res, req)
+	})
 
-func (lf *loginFlow) rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, rootHTML)
-}
+	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		t, _ := template.ParseFiles("templates/index.html")
+		t.Execute(res, false)
+	})
 
-func (lf *loginFlow) githubLoginHandler(w http.ResponseWriter, r *http.Request) {
-	// Generate a random state for CSRF protection and set it in a cookie.
-	state, err := randString(16)
-	if err != nil {
-		panic(err)
-	}
-
-	c := &http.Cookie{
-		Name:     "state",
-		Value:    state,
-		Path:     "/",
-		MaxAge:   int(time.Hour.Seconds()),
-		Secure:   r.TLS != nil,
-		HttpOnly: true,
-	}
-	http.SetCookie(w, c)
-
-	redirectURL := lf.conf.AuthCodeURL(state)
-	http.Redirect(w, r, redirectURL, http.StatusMovedPermanently)
-}
-
-func (lf *loginFlow) githubCallbackHandler(w http.ResponseWriter, r *http.Request) {
-	state, err := r.Cookie("state")
-	if err != nil {
-		http.Error(w, "state not found", http.StatusBadRequest)
-		return
-	}
-	if r.URL.Query().Get("state") != state.Value {
-		http.Error(w, "state did not match", http.StatusBadRequest)
-		return
-	}
-
-	code := r.URL.Query().Get("code")
-	tok, err := lf.conf.Exchange(context.Background(), code)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// This client will have a bearer token to access the GitHub API on
-	// the user's behalf.
-	client := lf.conf.Client(context.Background(), tok)
-	resp, err := client.Get("https://api.github.com/user")
-	if err != nil {
-		panic(err)
-	}
-	respbody, _ := io.ReadAll(resp.Body)
-	userInfo := string(respbody)
-
-	w.Header().Set("Content-type", "application/json")
-	fmt.Fprint(w, string(userInfo))
-}
-
-// randString generates a random string of length n and returns its
-// base64-encoded version.
-func randString(n int) (string, error) {
-	buf := make([]byte, n)
-	if _, err := rand.Read(buf); err != nil {
-		return "", err
-	}
-	return base64.RawURLEncoding.EncodeToString(buf), nil
+	log.Println("listening on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", p))
 }
--- go.mod
+++ go.mod
@@ -3,13 +3,19 @@ module github.com/syntaqx/playground
 go 1.21.5
 
 require (
+	github.com/gorilla/pat v1.0.2
+	github.com/gorilla/sessions v1.2.2
 	github.com/joho/godotenv v1.5.1
-	golang.org/x/oauth2 v0.15.0
+	github.com/markbates/goth v1.78.0
 )
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/context v1.1.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
 	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/oauth2 v0.15.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
